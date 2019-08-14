f04409e3659ef02a8bd24bdb83359e2eb863df0f
yejiCho <51953636+yejiCho@users.noreply.github.com>
2019-08-14 21:14:07 +0900

    Django
    Djangi

--- museum/main/models.py
+++ museum/main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+# 모델 클래스
+# 상품 후보 등록
+# django에서는 model이름은 대문자, 단수형
 # Create your models here.
+class Museum(models.Model):
+    name = models.CharField(max_length=10)
+    introduction = models.TextField()
+    area = models.CharField(max_length=15)
+    free = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
--- museum/main/views.py
+++ museum/main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Museum  #models에 정의된 Museum을 import
 
 def index(request):
     return render(request, 'main/index.html')
